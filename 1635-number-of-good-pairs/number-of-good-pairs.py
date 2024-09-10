320dcedc7329c59b3763a11683495ba7331f556c
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-09-10 23:10:07 +0300

    Time: 43 ms (14.25%) | Memory: 16.5 MB (19.79%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,9 +1,9 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         c = 0
-        s = defaultdict(int)
+        st = defaultdict(int)
         for a in nums:
-            s[a]+=1
-            c += s[a]-1
+            st[a]+=1
+            c += st[a]-1
         return c
         
