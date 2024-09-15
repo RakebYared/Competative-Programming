97df38068359019f232fc3184fe95af26ad735a7
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-09-15 13:01:16 +0300

    Time: 114 ms (57.03%) | Memory: 18.4 MB (81.83%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, h: List[int]) -> int:
+        l, r, add, con= 0, len(h)-1, 0, 0
+        while l<r:
+            con = max(con, min(h[l],h[r]))
+            while l<r:
+                if h[l]<=h[r]:
+                    l+=1
+                    if h[l]<con: add+= (con-h[l])  
+                    else: break
+                else:
+                    r-=1
+                    if h[r]<con: add+= (con-h[r])  
+                    else: break
+        return add
+
+        
