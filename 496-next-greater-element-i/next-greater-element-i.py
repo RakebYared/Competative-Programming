23a7a326048054a095ec9113b04047c375bb9d16
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-09-18 14:19:25 +0300

    Time: 55 ms (33.13%) | Memory: 16.9 MB (23.80%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,20 +1,14 @@
 class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack=[] #to store index of num2 that is in num1
-        lib = {}
+    def nextGreaterElement(self,nums1: List[int], nums2: List[int]) -> List[int]:
+        next_greater = {}
+        stack = []
+        for num in nums2:
+            while stack and stack[-1] < num:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+        for num in stack:
+            next_greater[num] = -1
 
-        for i in range(len(nums2)):
-            while stack and nums2[i]>nums2[stack[-1]]:
-                lib[nums2[stack[-1]]] = nums2[i]
-                stack.pop()
-            if nums2[i] in nums1: stack.append(i)
-        
-        for i in range(len(nums1)):
-            nums1[i] = lib.get(nums1[i],-1)
-        
-        return nums1
-
-
-
-            
-        
+        result = [next_greater[num] for num in nums1]
+    
+        return result
