aee9158b6b4731c1e44357a4b066426ddd5bf06b
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-10-03 12:26:20 +0300

    Time: 119 ms (78.73%) | Memory: 21.4 MB (9.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -8,7 +8,6 @@ class Solution:
             b = interval[i][1]
             while i<len(interval)-1 and b>=interval[i+1][0]:
                 i+=1
-                a = min(a, interval[i][0])
                 b = max(b, interval[i][1])
             res.append([a,b])
             i+=1
