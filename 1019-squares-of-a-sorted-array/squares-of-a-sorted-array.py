4186f242da994e4f29ee684ca2474d4acc30ef21
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-10-10 12:10:12 +0300

    Time: 153 ms (67.03%) | Memory: 19 MB (5.60%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -3,9 +3,6 @@ class Solution:
         l, r= 0, len(nums)-1
         ans = [0]*len(nums)
         for i in range(len(nums)-1,-1,-1):
-            if l==r:
-                ans[i] = pow(nums[l],2)
-                return ans
             if abs(nums[l]) > abs(nums[r]):
                 ans[i] = pow(nums[l],2)
                 l+=1
@@ -13,5 +10,5 @@ class Solution:
                 ans[i] = pow(nums[r],2)
                 r-=1
 
-
+        return ans
         
