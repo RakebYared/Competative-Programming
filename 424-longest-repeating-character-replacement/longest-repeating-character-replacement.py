ddfc7de45a31f1b54dcc0efb2fab91d325f00fff
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-10-16 16:04:37 +0300

    Time: 149 ms (14.55%) | Memory: 16.8 MB (10.71%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,60 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        store = defaultdict(int)
+        l,r,maxcount = 0, 0, 0
+        counter = 0
+        while r<len(s):
+            while r<len(s):
+                store[s[r]]+=1
+                counter +=1
+                if counter-max(store.values())>k:
+                    break
+                r+=1
+            maxcount = max(maxcount, r-l)
+            while counter-max(store.values())>k:
+                store[s[l]]-=1
+                counter-=1
+                l+=1
+            r+=1
+        return maxcount
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        r, l, maxcount, oddone = 0, 0, 0, 0
+
+        while r<=len(s):
+            while r<len(s):
+                if s[r]!=s[l]:
+                    oddone+=1
+                if oddone>k:
+                    break
+                r+=1
+            maxcount = max(maxcount, r-l)
+            while r<len(s) and l<r and s[l]!=s[r]:
+                l+=1
+            oddone-=1
+            r+=1
+
+        return maxcount
+
+
+        
