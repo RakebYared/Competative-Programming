5fe6ad1d23288b3f332d6049ec3feeeca313b50b
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-11-15 18:19:50 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (25.37%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,14 +1,10 @@
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
-        l, i, r = 0, 0, len(nums) - 1
+        for i in range(len(nums)):
+            smallest = i
+            for j in range(i+1, len(nums)):
+                if nums[j]<nums[smallest]:
+                    smallest = j
+            nums[smallest], nums[i] = nums[i], nums[smallest]
+        return nums
         
-        while i <= r:
-            if nums[i] == 0:
-                nums[l], nums[i] = nums[i], nums[l]
-                l += 1
-                i += 1
-            elif nums[i] == 1:
-                i += 1
-            else:
-                nums[i], nums[r] = nums[r], nums[i]
-                r -= 1
