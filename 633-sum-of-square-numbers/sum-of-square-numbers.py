4bdbef2e1b9b22ec26ce49f3a4e032eb3cde7070
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2024-12-10 21:19:03 +0300

    Time: 119 ms (21.04%) | Memory: 17.4 MB (23.78%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,14 +1,27 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
 # 0 1 3.......-2 -1 sqrt(c) o(n): 
-
         l, r = 0, int(sqrt(c))
         while l<=r:
-            res = l*l + (r*r)
-            if res>c:
-                r-=1
-            elif res<c:
-                l+=1
-            else:
+            m = (l+r)//2
+            rem = sqrt(c - (m*m))
+            if rem == int(rem):
                 return True
+            l+=1
         return False
+'''
+8^ + 6^ =100
+0  .....  10 
+m 5 : rem = 100-25 = 75 = 8.66 but its not int but since its > than m l=m+1
+m 8 : rem = 100-64 = 36 = 6 since 6 is int and < 10 
+
+0 ... 7
+m 3: rem = 50-9=41 = 6.4  l=m+1 bc rem > m
+m 5: rem = 5 
+
+0...4
+m = 2, rem = 16-4=12= 3.4 rem>m
+m = 3, rem = 16-9= 7 = 2.6 
+
+'''
+
