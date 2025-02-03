c071868f0bf851b6262662bf6dea69050b9b0f68
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-02-03 21:09:55 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (7.65%) - LeetSync

--- 858-masking-personal-information/masking-personal-information.py
+++ 858-masking-personal-information/masking-personal-information.py
@@ -1,15 +1,15 @@
 class Solution:
     def maskPII(self, s: str) -> str:
+        
         if '@' in s:
             s = s.lower()
             name, domain = s.split('@')
             return name[0]+ '*****' +name[-1]+'@'+domain
+
         else:
             separator = {'+', '-', '(', ')', ' '}
-            number = []
-            for i in range(len(s)):
-                if s[i] not in separator:
-                    number.append(s[i]) 
+            number = [a for a in s if a not in separator] 
+            
             number = ''.join(number)
             if len(number) == 10:
                 return '***-***-' + number[len(number)-4:]
