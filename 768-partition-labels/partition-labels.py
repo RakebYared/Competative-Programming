dd105029363bfdcd8b34b814465ec6bf1b12a2eb
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-02-12 17:00:16 +0300

    Time: 6 ms (46.33%) | Memory: 17.6 MB (79.80%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,20 +1,28 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        interval = defaultdict(list)
-        for i in range(len(s)):
-            if interval[s[i]]:
-                interval[s[i]][1] = i
-            else:
-                interval[s[i]] = [i,i]
-        interval = list(interval.values())
-        res = []
-        i = 0
-        while i<len(interval):
-            start = interval[i][0]
-            end = interval[i][1]
-            while i<len(interval) and interval[i][0] <= end:
-                end = max(end, interval[i][1])
-                i+=1
-            res.append(end-start+1)
 
-        return res
+        location = defaultdict(int)
+
+        for i in range(len(s)):
+            location[s[i]] = i
+        
+        ans = []
+        start = 0
+
+        while start < len(s):
+            end = location[s[start]]
+            i = start
+
+            while i < end and end < len(s):
+                if location[s[i]] > end:
+                    end = location[s[i]]
+                i += 1
+
+            ans.append(end - start + 1)
+            start = end + 1
+        
+        return ans
+        
+
+
+
