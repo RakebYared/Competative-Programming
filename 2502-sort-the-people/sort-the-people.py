6881d695586a97b30b5dcb7f16ab067e3018d903
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-02-12 09:37:17 +0300

    Time: 7 ms (43.85%) | Memory: 18.2 MB (66.37%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,7 +1,9 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        store = [""]* (max(heights)- min(heights)+1)
-        factor = max(heights)
-        for i, a in enumerate(heights):
-            store[factor-a] = names[i]
-        return [name for name in store if name != ""]
+
+        person = [list(character) for character in zip(names, heights)]
+        ans = [name[0] for name in sorted(person, key = lambda x : x[1], reverse = True)]
+        return ans
+
+
+    
