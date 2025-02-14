3ee4402588077dc9c2e4f3f7753a4c19b19fdea4
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-02-14 10:57:39 +0300

    Time: 7 ms (99.79%) | Memory: 17.9 MB (38.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,21 +1,29 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1list = [0]*26
-        for a in s1:
-            s1list[ord(a)-97]+=1
+        counter1 = [0]*26
+        counter2 = [0]*26
+        k = len(s1)
+        n = len(s2)
 
-        l = 0
-        while l < len(s2):
-            r = l
-            copylist = s1list.copy()
-            while r<len(s2) and copylist[ord(s2[r])-97]-1>=0:
-                copylist[ord(s2[r])-97]-=1
-                r+=1
-            if r-l == len(s1):
-                return True
-            if r==len(s2): break
-            while l<r and s2[l]!=s2[r]:
-                l+=1
-            l+=1
+        for letter in s1:
+            counter1[ord(letter) - 97] += 1
+        
+        for letter in s2[:k]:
+            counter2[ord(letter) - 97] += 1
+        
+        if counter1 == counter2:
+            return True 
+                    
+        for i in range(n-k):
+            counter2[ord(s2[i]) - 97] -= 1
+            counter2[ord(s2[i + k]) - 97] += 1
+
+            if counter1 == counter2:
+                return True 
+            
         return False
+   
 
+        
+       
+        
