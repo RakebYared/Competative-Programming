9a9d3706dda3f9724798db760090e3add3894f6d
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-03-14 09:39:06 +0300

    Time: 3 ms (65.99%) | Memory: 18 MB (30.62%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,13 +1,19 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
+        dp = {}
         def play(l, r):
+            if (l,r) in dp:
+                return dp[(l,r)]
+
             if l == r:
                 return nums[l]
 
             right = nums[r] - play(l,r-1)
             left = nums[l] - play(l+1, r)
 
-            return max(right, left)
+            dp[(l,r)] = max(right, left) 
+
+            return dp[(l,r)]
         
         return play(0,len(nums)-1) >= 0
 
