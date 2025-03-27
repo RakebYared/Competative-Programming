273f5bbf1c591e13bc3a62d5dd4288fed01afc64
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-03-27 09:26:04 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (15.00%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans = []
+
+
+        def compute(val, store):
+            if len(val) == n:
+                ans.append(val.copy())
+            
+            nums = list(store)
+
+            for num in nums:
+                store.remove(num)
+                val.append(num)
+                compute(val, store)
+                store.add(num)
+                val.pop()
+            
+        compute([], set(nums))
+        return ans
+
+
+        
