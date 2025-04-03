db0bbf53fa69053fa497c532a5c12a365bc698d9
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-04-03 12:37:33 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.04%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,23 +1,11 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        """s = list(map(str, nums))
-        max_place = len(max(s, key=len))
-
-        s = sorted(s, key = lambda x: x + x[-1]*(max_place-len(x)), reverse = True)
-        return ''.join(s)
-        """
-
-        nums = list(map(str, nums))
-
-        # bubble sort
-        for i in range(len(nums)):
-            for j in range(1, len(nums)-i):
-                if nums[j-1] + nums[j] < nums[j] + nums[j-1]:
-                    nums[j], nums[j-1] = nums[j-1], nums[j]
-        
-        if nums == ['0']*len(nums):
-            return "0"
-            
-        return ''.join(nums)
+    
+        nums = [str(num) for num in nums]
+        nums.sort(key = lambda x: x*(11-len(x)), reverse = True)
+        nums = ''.join(nums)
+        if nums == '0'*len(nums):
+            return '0'
+        return nums
 
 
