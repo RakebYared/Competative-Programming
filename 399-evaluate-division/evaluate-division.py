d20c0abaa1b9344c779820f4a894aef93ea23650
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-04-08 20:15:19 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (49.10%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -7,30 +7,28 @@ class Solution:
             nume, deno = val
             graph[nume].append([deno, values[i]])
             graph[deno].append([nume, 1/values[i]])
-        
+        visited = set()
+        des = -1
 
-        def dfs(vertix, visited, weight, des):
-    
+        def dfs(vertix):
             visited.add(vertix)
-
-            
             if vertix == des:
-                return weight 
-
-            for n,w in graph[vertix]:
+                return 1 
+            for n, w in graph[vertix]:
                 if n not in visited:
-                    ans = dfs(n, visited, weight * w, des)
+                    ans = dfs(n)
                     if ans != -1:
-                        return ans
-            
+                        return ans*w
             return -1
 
         ans = []
 
-        for source, des in queries:
-            if source not in graph or des not in graph:
+        for source, de in queries:
+            if source not in graph or de not in graph:
                 ans.append(-1)
             else:
-                ans.append(dfs(source, set(), 1, des))
+                visited = set()
+                des = de
+                ans.append(dfs(source))
         
         return ans
