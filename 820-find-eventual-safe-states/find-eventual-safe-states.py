fff226fbde77e49326c7116deef30ab18cca50af
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-04-25 10:38:49 +0300

    Time: 47 ms (53.94%) | Memory: 23.6 MB (75.83%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -2,37 +2,38 @@ class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
 
-        dp = ['']*n
-        
-        safe_node = []
-
-        def dfs(node):
-
-            if dp[node]:
-                return True if dp[node] == '1' else False
-
-            dp[node] = 'g' 
-
-            if not len(graph[node]):
-                dp[node] = '1'
-                return True
-
-            for nei in graph[node]:
-                if not dfs(nei) or dp[nei] == 'g':
-                    dp[node] = '0'
-                    return False
-            
-            dp[node] ='1'
-            return True
+        temp = [[] for _ in range(n)]
+        indegree = [0]*n
 
         for node in range(n):
 
-            if dfs(node):
-                safe_node.append(node)
+            for child in graph[node]:
+                temp[child].append(node)
+            
+            indegree[node] = len(graph[node])
 
-        return safe_node
-
-                
-
-
+        graph = temp
         
+        q = deque()
+
+        for node in range(n):
+            if not indegree[node]:
+                q.append(node)
+
+
+        ans = []
+
+        while q:
+
+            node = q.pop()
+            ans.append(node)
+
+            for nei in graph[node]:
+                indegree[nei] -= 1
+
+                if not indegree[nei]:
+                    q.append(nei)
+
+        return sorted(ans)
+
+       
