357c440ec44796975fe529211199ed7fe819d480
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-05-16 18:48:04 +0300

    Time: 276 ms (50.77%) | Memory: 20.5 MB (22.69%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,33 +1,25 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
 
-        bit = [0, 0, 0, 0, 0]
         index = {letter:i for letter, i in zip('aeiou', [0,1,2,3,4])}
 
         store = defaultdict(int)
-        store[(0,0,0,0,0)] = -1
+        store[0] = -1
 
+        state = 0
         ans = 0
 
 
         for i,letter in enumerate(s):
 
             if letter in index: 
-                bit[index[letter]] += 1 
-                bit[index[letter]] %= 2
-
-
+                state ^= (1 << index[letter])
             
-            check = tuple(bit)
-
-
-
-            if check not in store:
-                store[check] = i
-           
-
+            if state in store:
+                ans = max(ans, i - store[state])
             else:
-                ans = max(ans,  i - store[check])
+                store[state] = i
+              
             
 
         return ans
