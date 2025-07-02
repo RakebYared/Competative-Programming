4aa92a3bc507b92135e15f7dd2b94dbed9b1669f
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-07-02 13:15:36 +0300

    Time: 807 ms (66.65%) | Memory: 90.4 MB (48.54%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,55 +1,36 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        # kruskal algo
+        # prim algo
 
         n = len(points)
-        graph = [] # [weigth, x, y]
-
-        for i in range(n):
-            for j in range(i+1, n):
-
-                x, y = points[i]
-                xi, yi = points[j]
-
-                dis = abs(x - xi) + abs(y - yi)
-
-                graph.append([dis, i, j])
         
-        graph.sort()
+        heap = [[0, 0]] # [cost, node]
+        vis = set()
 
-        parent = [i for i in range(n)]
-        size = [1]*n
-
-        def find(x):
-
-            while x != parent[x]:
-
-                parent[x] = parent[parent[x]]
-                x = parent[x]
-            
-            return x
-
-        def union(x, y):
-            x = find(x)
-            y = find(y)
-
-            if x == y:
-                return True
-
-            if size[x] < size[y]:
-                x, y = y, x
-
-            parent[y] = x
-            size[x] += size[y]
-            
         ans = 0
 
-        for dis, x, y in graph:
+        while len(vis) < n:
 
-            if not union(x,y):
-                ans += dis 
+            cost, node = heappop(heap)
+
+            if node in vis:
+                continue
+            
+            vis.add(node)
+            ans += cost
+            x, y = points[node]
+
+            for nei in range(n):
+
+                if nei not in vis:
+
+                    xi, yi = points[nei]
+                    dis = abs(x - xi) + abs(y - yi)
+                    heappush(heap, [dis, nei])
         
         return ans
+
+
 
 
 
