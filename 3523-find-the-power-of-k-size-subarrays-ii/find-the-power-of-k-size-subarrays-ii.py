63d99bbb8ab9c6e6ecf12f2605e8f726ff58d477
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-09-25 12:16:10 +0300

    Time: 94 ms (87.65%) | Memory: 34.8 MB (26.47%) - LeetSync

--- 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.py
+++ 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.py
@@ -1,29 +1,24 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-
-        q = deque()
         n = len(nums)
-
-        for i in range(k):
-            while q and (q[-1][0] >= nums[i] or abs(q[-1][0] - nums[i]) > 1):
-                q.pop()
-            q.append([nums[i], i])
-        
         ans = []
-        ans.append(-1 if len(q) != k else q[-1][0])
+        count = 1
 
-        for i in range(n-k):
-            if q[0] == [nums[i], i]:
-                q.popleft()
+        if k == 1:
+            ans.append(nums[0])
+
+        for i in range(1,n):
+            if nums[i] - nums[i-1] == 1:
+                count += 1
+            else:
+                count = 1
             
-            while q and ((q[-1][0] >= nums[i+k]) or abs(q[-1][0] - nums[i+k]) > 1 ):
-                q.pop()
-            q.append([nums[i+k], i+k])
-        
-       
-            ans.append(-1 if len(q) != k else q[-1][0])
+            if i+1 >= k:
+                if count >= k:
+                    ans.append(nums[i])
+                else:
+                    ans.append(-1)
+
         return ans
+            
 
-
-    
-        
