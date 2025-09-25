b310938a7cbe541f15e926dbbde6029f385eedaa
Rakeb Yared <128320255+RakebYared@users.noreply.github.com>
2025-09-25 11:56:49 +0300

    Time: 359 ms (5.30%) | Memory: 37.3 MB (5.29%) - LeetSync

--- 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.py
+++ 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.py
@@ -1,39 +1,29 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
 
-        maxx = deque()
-        minn = deque()
+        q = deque()
         n = len(nums)
 
         for i in range(k):
-            while maxx and (nums[i] <= maxx[-1][0] or abs(nums[i] - maxx[-1][0]) > 1 ):
-                maxx.pop()
-            maxx.append([nums[i], i])
-
-            while minn and (nums[i] >= minn[-1][0] or abs(nums[i] - minn[-1][0]) > 1 ):
-                minn.pop()
-            minn.append([nums[i], i])
-
+            while q and (q[-1][0] >= nums[i] or abs(q[-1][0] - nums[i]) > 1):
+                q.pop()
+            q.append([nums[i], i])
+        
         ans = []
-        ans.append(maxx[-1][0] if len(maxx) == k else -1)
+        ans.append(-1 if len(q) != k else q[-1][0])
 
-        for i in range(n - k):
+        for i in range(n-k):
+            if q[0] == [nums[i], i]:
+                q.popleft()
             
-            if minn[0] == [nums[i],i]:
-                minn.popleft()
-            if maxx[0] == [nums[i], i]:
-                maxx.popleft()
-
-            while maxx and (nums[i+k] <= maxx[-1][0] or abs(nums[i+k] - maxx[-1][0] ) > 1) :
-                maxx.pop()
-            maxx.append([nums[i+k], i+k])
-
-            while minn and (nums[i+k] >= minn[-1][0] or abs(nums[i+k] - minn[-1][0]) > 1 ):
-                minn.pop()
-            minn.append([nums[i+k], i+k])
-
-
-            ans.append(maxx[-1][0] if len(maxx) == k else -1)
-
+            while q and ((q[-1][0] >= nums[i+k]) or abs(q[-1][0] - nums[i+k]) > 1 ):
+                q.pop()
+            q.append([nums[i+k], i+k])
+        
+       
+            ans.append(-1 if len(q) != k else q[-1][0])
         return ans
+
+
+    
         
